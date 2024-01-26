335f0d3ceebaf7ab50be7e730fdfa4915c7a6463
Vsevolod Yablonovskiy <s0e2va@mail.ru>
2024-01-27 00:29:48 +0500

    Menu

--- game_menu/menu.gd
+++ game_menu/menu.gd
@@ -1,5 +1,4 @@
-extends Node2D
-
+extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +8,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://game_menu/anecdotes.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
