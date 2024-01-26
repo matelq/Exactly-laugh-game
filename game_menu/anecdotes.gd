c5c1204d5c17fd1df465d598422f4e9850c4c810
Vsevolod Yablonovskiy <s0e2va@mail.ru>
2024-01-27 00:47:17 +0500

    Dialog manager and anecdotes

--- game_menu/anecdotes.gd
+++ game_menu/anecdotes.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	update_anecdote()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +12,10 @@ func _process(delta):
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://game_menu/menu.tscn")
+
+
+func _on_next_button_pressed():
+	update_anecdote()
+	
+func update_anecdote():
+	pass
