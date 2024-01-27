71cc86587a0513d81a9d29dc2ae84051368481d3
matelq <109665667+matelq@users.noreply.github.com>
2024-01-28 04:01:10 +0500

    trash kill eoeeee

--- src/emeny/enemy.gd
+++ src/emeny/enemy.gd
@@ -4,6 +4,7 @@ signal collided_with_staticBody
 signal damage_taken(value)
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var isGravity = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,11 +15,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if not is_on_floor():
+	if not is_on_floor() and isGravity:
 		velocity.y += gravity * delta
-		
-	move_and_slide()
-	process_collisions()
+		move_and_slide()
+		process_collisions()
+	
 
 func look_left():
 	$Idle.hide()
--- src/locations/resto/Resto_inside.gd
+++ src/locations/resto/Resto_inside.gd
@@ -4,19 +4,14 @@ var isNextSceneCollide = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
+	$progress_label.hide()
 
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if isNextSceneCollide:
-		$progress_label.show()
-	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://src/locations/parking_lot_peacefull/ParkingLotPeacefull.tscn") 
-	else:
-		$progress_label.hide()
+
 	
 	if Input.is_action_just_pressed("E"):
 		$Code.show()
