4a9f03941e38ec0ce1706b1c5c17f6c4bf70b4b9
matelq <109665667+matelq@users.noreply.github.com>
2024-01-28 14:01:04 +0500

    anecdotes done

--- src/game_menu/anecdotes.gd
+++ src/game_menu/anecdotes.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
+@export var aneci_speed = 75
+var is_aneci_move = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	update_anecdote()
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if is_aneci_move:
+		$Anecdore.position.y -= delta*aneci_speed
 	pass
+
+func stop_moving():
+	is_aneci_move = false
+	pass
+
+func continue_moving():
+	is_aneci_move = true
 
 
 func _on_back_button_pressed():
@@ -18,4 +28,18 @@ func _on_next_button_pressed():
 	update_anecdote()
 	
 func update_anecdote():
+	var dialog = load("res://src/anecdotes.dialogue")
+	DialogueManager.show_example_dialogue_balloon(dialog, "razgovor")
 	pass
+
+func stop_audi():
+	$AudioStreamPlayer.stop()
+	pass
+
+func start_audi():
+	$AudioStreamPlayer.play()
+	pass
+
+func _on_audio_stream_player_finished():
+	$AudioStreamPlayer.play()
+	pass # Replace with function body.
